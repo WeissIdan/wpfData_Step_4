4b08619170651f792e8038e79b93deca7b80b646
idan_weiss <i.weiss.2006@gmail.com>
2023-11-02 08:05:19 +0200

    Add project files.

---
+++ wpfData/LoginWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using wpfData_Step_4.Model;
+using wpfData_Step_4.ViewModel;
+
+namespace wpfData_Step_4
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            UserDB Udb = new UserDB();
+            User newUser = new User();
+            newUser.Password = tbxPassword.Password;
+            newUser.UserName = tbxID.Text;
+            User afterLogin = Udb.Login(newUser);
+            if (afterLogin == null)
+            {
+                MessageBox.Show("error logging in, check values");
+            }
+            else
+            {
+                if (afterLogin.IsAdmin)
+                {
+                    MainWindow mainW = new MainWindow(afterLogin);
+                    mainW.Show();
+                }
+                else
+                {
+                    MessageBox.Show("successful login, but you are not an admin");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ wpfData/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wpfData;
+using wpfData_Step_4.Model;
+
+namespace wpfData_Step_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        User currentUser;
+        public MainWindow(User user)
+        {
+            InitializeComponent();
+            currentUser = user;
+        }
+
+        private void City_load()
+        {
+            dataDockPanel.Children.Clear();
+            dataDockPanel.Children.Add(new CityUserControl());
+        }
+        private void Users_load()
+        {
+            dataDockPanel.Children.Clear();
+            dataDockPanel.Children.Add(new UserUserControl1());
+        }
+
+        private void goToLogin(object sender, RoutedEventArgs e)
+        {
+            LoginWindow lgw = new LoginWindow();
+            lgw.ShowDialog();
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string name = ((MenuItem)sender).Header.ToString();
+                dataDockPanel.Children.Clear();
+                switch (name)
+                {
+                    case "City":
+                        City_load();
+                        break;
+                    case "Users":
+                        Users_load();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        
+    }
+}
